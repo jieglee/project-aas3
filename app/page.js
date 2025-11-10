ec2887c5df66fbeaaa1d737d263eb15ccaed2b67
jieffrey <kyooon08@gmail.com>
2025-11-10 22:38:38 +0700

    Perbaikan konflik dan gabung versi terbaru

--- app/page.js
+++ app/page.js
@@ -1,5 +1,19 @@
-import LandingPage from "./component/LandingPage/page";
+import Tentang from "./component/About/page";
+import Koleksi from "./component/Collection/page";
+import Contact from "./component/Contact/page";
+import Fitur from "./component/Fiturr/page";
+import Hero from "./component/Hero/page";
+import Statistik from "./component/Statistic/page";
 
 export default function Home() {
-  return <LandingPage />;
+  return (
+    <div className="flex flex-col items-center w-full overflow-hidden bg-white">
+      <Hero/>
+      <Statistik/>
+      <Tentang/>
+      <Koleksi/>
+      <Fitur/>
+      <Contact/>
+    </div>
+  )
 }
